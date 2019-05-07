165e2059b57aaa0a4176145fd2cbc29e6b2a551e
honghaitzz11 <honghaitzz11@gmail.com>
2019-05-07 21:39:27 +0800

    ~/help/index

--- 05-makeMobi.js
+++ 05-makeMobi.js
@@ -1,5 +1,5 @@
 const shell = require('shelljs');
 const utils = require('./utils/base');
 shell.exec(`zip -p -r ${utils.name}.epub book`);
-shell.exec(`/Users/honghaitao/Applications/KindleGen_Mac_i386_v2_9/kindlegen -c1 ${utils.name}.epub -locale zh`);
+shell.exec(`./utils/kindlegen -c1 ${utils.name}.epub -locale zh`);
 shell.exec(`rm ${utils.name}.epub`);
